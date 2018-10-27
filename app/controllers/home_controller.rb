6a4599ca3de352b2bed5adddf6bac88de9119813
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2018-10-27 04:27:09 -0300

    Got data from API and started building a search form.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
+
+    #getting Coinmarketcap info
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+
+    @mycoins = ['BTC', 'BCH', 'XRP', 'ETH']
+    
+  end
+
+  def lookup
+    @symbol = params[:sym]
   end
 end
